3df119bea012b331b01a6b9cbbadb92117010678
mohamed501234 <hama.bennacef50@gmail.com>
2023-08-14 17:36:30 +0100

    change home and NavBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,29 +4,29 @@ import ProductDetails from './components/ProductDetails'
 import Home from './components/Home'
 import axios from 'axios'
 import { useEffect, useState } from 'react';
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import Navbars from "./components/Navbars"
 
 
 function App() {
-  const [data, setData] = useState([]); //! MAIN DATA
-  const [selected,setSelected] = useState({}); //! one product selected for the detailed
-  console.log(data);
+  const [data, setData] = useState([]);
+  const [selected, setSelected] = useState({});
 
   useEffect(()=>{
     axios.get('http://localhost:4000/api/phones')
-    .then((response)=>setData(response.data))
-    .catch((error)=>console.log(error))
+      .then((response)=>setData(response.data))
+      .catch((error)=>console.log(error))
   },[])
-
   return (
-    <div className="App">
-      <BrowserRouter>
+    <BrowserRouter>
+    <Navbars/>
         <Routes>
-          <Route exact path="/" element={<Home data={data}/>}/>
+          <Route exact path="/" element={<Home data={data} setSelected={setSelected} />} />
+          <Route path="/cart" element={<CartList />} />
+          <Route path="/ProductDetails" element={<ProductDetails selected={selected} />} />
         </Routes>
-      </BrowserRouter>
-
-    </div>
+    
+    </BrowserRouter>
   );
 }
 
